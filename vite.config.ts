d39ca4b2ae319e204c4e43f084440b91711414ad
strayer <1311140141@qq.com>
2026-05-18 12:12:03 +0800

    chore: github部署静态页面

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base:'/process-draw/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
